2af979d3026b549d9e72c5b047c6dfec41063dd3
John Reed <johnr@loogatee.com>
2018-05-28 23:09:11 -0700

    final? fix to cover_ base addr

--- x.sh
+++ x.sh
@@ -4,8 +4,7 @@
 rm -rf ./public
 hugo
 
-mkdir -p public/user1/docs/img
-mkdir -p public/user1/docs/user1/img
+mkdir -p public/user1/img
 
 #
 #   Find all names beginning in cover_
@@ -15,9 +14,8 @@ mkdir -p public/user1/docs/user1/img
 #
 #   This works around some link-generation bugs in hugo
 
-find content -name cover_* -exec cp {} ./public/img                   \;
-find content -name cover_* -exec cp {} ./public/user1/docs/img        \;
-find content -name cover_* -exec cp {} ./public/user1/docs/user1/img  \;
+find content -name cover_* -exec cp {} ./public/img              \;
+find content -name cover_* -exec cp {} ./public/user1/img        \;
 
 
 cp ./public/img/README.txt ./public/README.md
